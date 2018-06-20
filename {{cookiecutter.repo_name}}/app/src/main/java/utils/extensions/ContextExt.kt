dd6d6bc0e70a0e55c3206bb11db27429045acba6
Mehmet Ayan <m.salimayan@gmail.com>
2018-06-20 17:18:10 +0300

    Revert "extensions functions are added"

--- {{cookiecutter.repo_name}}/app/src/main/java/utils/extensions/ContextExt.kt
+++
@@ -1,7 +0,0 @@
-package {{ cookiecutter.package_name }}.utils.extensions
-
-import android.content.Context
-import android.support.annotation.ColorRes
-import android.support.v4.content.ContextCompat
-
-fun Context.getColorCompat(@ColorRes resourceId: Int) = ContextCompat.getColor(this, resourceId)
